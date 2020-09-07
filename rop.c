bf7fc3c705fdbb438a0ff154647911841f0bf711
Qixiang Xu <qixiang.xu@arm.com>
2020-09-07 15:54:44 +0800

    add rop

--- rop.c
+++ rop.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void rop_symbol()
+{
+	system("/bin/ls");
+	printf("%s\n","/bin/sh");
+}
+
+void rop_bad_func()
+{
+	char data[16] = {0}; 
+	unsigned long long u64 = 0; 
+	int fd = 0; 
+	fd = open("./rop.data",O_RDONLY); 
+	read(fd,&u64,512);
+}
 
 int main()
 {
+	rop_bad_func();
 	return 0;
 }
+
+
